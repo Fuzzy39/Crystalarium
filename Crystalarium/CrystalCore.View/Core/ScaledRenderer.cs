64490ae1acca18059eda720697ef7d29877068ca
qwerty4967 <jmcraft126@gmail.com>
2023-04-03 21:10:27 -0400

    Create ScaledRenderer.cs

---
+++ Crystalarium/CrystalCore.View/Core/ScaledRenderer.cs
@@ -0,0 +1,24 @@
+﻿using CrystalCore.Util.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalCore.View.Core
+{
+    internal class ScaledRenderer : IRenderer
+    {
+        public void Draw(Texture2D texture, RotatedRect position, Rectangle sourceRect, Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawString(FontFamily font, string text, Vector2 position, float height, Color color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
